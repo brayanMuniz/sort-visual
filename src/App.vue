bc5f3225f970529e4f0199e061fdeed2377818ac
Brayan Muniz <brayanmuniz2636@gmail.com>
2020-07-31 13:07:59 -0700

    bar comp, fill method, render in numbers

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,46 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <Bar v-for="(num, idx) in currentOrder" :key="idx" :BarValue="num" />
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import { Vue, Component, Emit, Prop } from "vue-property-decorator";
+import Bar from "@/components/Bar.vue";
+import "vue-class-component/hooks";
 
 @Component({
   components: {
-    HelloWorld,
+    Bar,
   },
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  pastOrder: Array<Array<number>> = []; // this will be the history of all the mutations in the array
+  currentOrder: Array<number> = [];
+
+  mounted() {
+    this.currentOrder = this.generateRandomNumbers();
+    this.pastOrder.push(this.currentOrder);
+    console.log(this.pastOrder);
+  }
+
+  // Methods
+  @Emit()
+  generateRandomNumbers(amountBars?: number, max?: number): Array<number> {
+    if (amountBars == undefined) amountBars = 10;
+    if (max == undefined) max = 10;
+
+    const result: Array<number> = [];
+    for (let i = 0; i < amountBars; i++) {
+      result.push(Math.floor(Math.random() * max));
+    }
+    return result;
+  }
+}
+
+// Dont even need to make a bar yet, just be able to line up the numbers and move them around in animate them in a way that shows the steps of each algorithim
+
+// Be able to move the bars and pass in values before giving the sort order
+// make them green and such
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Bar.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>{{BarValue}}</div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from "vue-property-decorator";
+
+@Component
+export default class Bar extends Vue {
+  @Prop({ default: 0 }) readonly BarValue!: number;
+}
+</script>
